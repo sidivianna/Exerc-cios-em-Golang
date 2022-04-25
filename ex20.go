8575ad9e2ebeace736e52665b203ac915e1bfb1e
Sidinei Vianna <sidineirochavianna19@gmail.com>
2022-04-25 18:19:46 -0300

    Add files via upload
    Mais exercícios.

---
+++ ex20.go
@@ -0,0 +1,15 @@
+package main
+
+ import (
+	 "fmt"
+ )
+
+  func main() {
+	  slice := []int{10, 20, 30, 40, 50, 60, 70, 80, 90, 100}
+	  fmt.Println(slice[:3])
+	  fmt.Println(slice[4:])
+	  fmt.Println(slice[1:7])
+	  fmt.Println(slice[2:9])
+	  fmt.Println(slice[2:len(slice)-1])
+
+  }
